1326c75434012d3fdf8fe411d8b677cfc488545a
Kima <kazakova.k.040607@gmail.com>
2026-03-02 22:52:33 +0300

    Add new C++ program

---
+++ АВС_Лр2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <bitset>
+#include <cstring>
+using namespace std;
+
+int main() {
+    float x;
+    cout << "Enter number: ";
+    cin >> x;
+
+    unsigned int bits;
+    memcpy(&bits, &x, sizeof(x));
+
+    cout << "IEEE 754 (binary): " << bitset<32>(bits);
+    cout << "IEEE 754 (hex): " << hex << uppercase << bits;
+
+    return 0;
+}
